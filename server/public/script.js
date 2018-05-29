9b9e69b19f01a8d9482dc18a4c63060875692281
RadioColectivo <jarchila@radiocolectivo.com>
2018-05-29 09:31:06 -0400

    adding a chart

--- server/public/script.js
+++ server/public/script.js
@@ -33,3 +33,30 @@ setInterval(() => {
 	  fetchTemperature()
 	  fetchHumidity()
 }, 2000)
+
+/**
+*Get the context of the temperature canvas element
+**/
+const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d');
+
+/**
+*Create a new chart on the context we just instatiated
+**/
+
+const temperatureChart = new Chart(temperatureCanvasCtx, {
+	type: 'line',
+	data: {
+		labels: ['10:30', '10:31', '10:32', '10:33'],
+		datasets: [{
+			data:[12, 19, 23, 17],
+		backgroundColor: 'rgba(255, 205, 210, 0.5)'
+	}]
+	},
+	options: {
+		legend: {
+			display: false
+		},
+		responsive: true,
+		maintainAspectRatio: false
+	}
+})
