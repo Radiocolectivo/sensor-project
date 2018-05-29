89be5571512af927443299d6d53d7b4067f1c195
Javier Archila <jarchila@radiocolectivo.com>
2018-05-28 21:47:16 -0400

    make public dir

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const getCacheSensorReadings = require('./cache');
 
+	app.use('/public', express.static(path.join(__dirname, 'public')));
+
 	app.get('/temperature', function(req, res) {
-		res.send(getCacheSensorReadings.getTemperature().toFixed(1) + ' °C')
+		res.send('<strong>' +  getCacheSensorReadings.getTemperature().toFixed(1) + '</strong>' + ' °C')
 	});
 
 	app.get('/humidity', function(req, res) {
 		res.send(getCacheSensorReadings.getHumidity().toFixed(1) + ' %')
 	});
+	
 
 	app.listen(3000, function() {
 		console.log('Server listening on port 3000');
