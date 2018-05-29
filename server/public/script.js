4ac8e12ae4ade69c49449fd1192f7b6f1c8ee824
RadioColectivo <jarchila@radiocolectivo.com>
2018-05-29 09:44:15 -0400

    adding a chart2

--- server/public/script.js
+++ server/public/script.js
@@ -1,62 +1,105 @@
-/*
-* We put the code for fetching tmeperature in its own function
-*/
-	const fetchTemperature = () => {
-		  fetch('/temperature')
-		    .then(results => {
-			          return results.text()
-			        })
-		    .then(text => {
-			          const temperatureDisplay = document.getElementById('temperature-display')
-			          temperatureDisplay.innerHTML = text
-			        })
-	}
+const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d')
 
-/*
- * Make a similar function to fetch humidity
- * */
+const temperatureChartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: 'rgba(255, 205, 210, 0.5)'
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 10,
+          suggestedMax: 40
+        }
+      }]
+    }
+  }
+}
+const temperatureChart = new Chart(temperatureCanvasCtx, temperatureChartConfig)
+
+const humidityCanvasCtx = document.getElementById('humidity-chart').getContext('2d')
+
+const humidityChartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: 'rgba(197, 202, 233, 0.5)'
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 30,
+          suggestedMax: 90
+        }
+      }]
+    }
+  }
+}
+const humidityChart = new Chart(humidityCanvasCtx, humidityChartConfig)
+
+const pushData = (arr, value, maxLen) => {
+  arr.push(value)
+  if (arr.length > maxLen) {
+    arr.shift()
+  }
+}
+
+const humidityDisplay = document.getElementById('humidity-display')
+const temperatureDisplay = document.getElementById('temperature-display')
+
+const fetchTemperature = () => {
+  fetch('/temperature')
+    .then(results => {
+      return results.json()
+    })
+    .then(data => {
+      const now = new Date()
+      const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+      pushData(temperatureChartConfig.data.labels, timeNow, 10)
+      pushData(temperatureChartConfig.data.datasets[0].data, data.value, 10)
+      temperatureChart.update()
+      temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>'
+    })
+}
+
 const fetchHumidity = () => {
-	  fetch('/humidity')
-	    .then(results => {
-		          return results.text()
-		        })
-	    .then(text => {
-		          const temperatureDisplay = document.getElementById('humidity-display')
-		          temperatureDisplay.innerHTML = text
-		        })
+  fetch('/humidity')
+    .then(results => {
+      return results.json()
+    })
+    .then(data => {
+      const now = new Date()
+      const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+      pushData(humidityChartConfig.data.labels, timeNow, 10)
+      pushData(humidityChartConfig.data.datasets[0].data, data.value, 10)
+      humidityChart.update()
+      humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'
+    })
 }
 
 /*
- * Call the above defined functions at regular intervals
- * */
+Call the above defined functions at regular intervals
+*/
 setInterval(() => {
-	  fetchTemperature()
-	  fetchHumidity()
+  fetchTemperature()
+  fetchHumidity()
 }, 2000)
-
-/**
-*Get the context of the temperature canvas element
-**/
-const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d');
-
-/**
-*Create a new chart on the context we just instatiated
-**/
-
-const temperatureChart = new Chart(temperatureCanvasCtx, {
-	type: 'line',
-	data: {
-		labels: ['10:30', '10:31', '10:32', '10:33'],
-		datasets: [{
-			data:[12, 19, 23, 17],
-		backgroundColor: 'rgba(255, 205, 210, 0.5)'
-	}]
-	},
-	options: {
-		legend: {
-			display: false
-		},
-		responsive: true,
-		maintainAspectRatio: false
-	}
-})
